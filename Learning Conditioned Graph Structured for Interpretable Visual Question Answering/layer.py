3901fb5d19566f4c5e8194a2b307aa9dc5d00c13
junikuni <ghwns1878@naver.com>
2021-05-07 11:59:54 +0900

    add forward method to Spatial Graph Convolution

--- Learning Conditioned Graph Structured for Interpretable Visual Question Answering/layer.py
+++ Learning Conditioned Graph Structured for Interpretable Visual Question Answering/layer.py
@@ -35,15 +35,20 @@ class Spatial_Graph_Convolution(nn.Module):
 
     def __init__(self, input_dim, output_dim, num_kernel):
 
+        super(Spatial_Graph_Convolution, self).__init__()
+
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.num_kernel
+        self.num_kernel = num_kernel
+
+        #layers
+        self.kernel_layers = nn.ModuleList([nn.Linear(input_dim, output_dim // num_kernel, bias=False) for i in range(num_kernel)])
         
         #Gaussian_kernel trainable parameters
-        self.mean_length = Parameter(torch.tensor(num_kernel, 1))
-        self.sd_length = Parameter(torch.tensor(num_kernel, 1))
-        self.mean_angle = Parameter(torch.tensor(num_kernel, 1))
-        self.sd_length = Parameter(torch.tensor(num_kernel, 1))
+        self.mean_length = Parameter(torch.Tensor(num_kernel, 1))
+        self.sd_length = Parameter(torch.Tensor(num_kernel, 1))
+        self.mean_angle = Parameter(torch.Tensor(num_kernel, 1))
+        self.sd_length = Parameter(torch.Tensor(num_kernel, 1))
 
 
         
@@ -52,13 +57,30 @@ class Spatial_Graph_Convolution(nn.Module):
         
         """
         in_feat - [batch * num_proposal, num_neighbor, in_feat_dim]
-        polar_coordinate - [batch * num_proposal, num_neighbor, in_feat_dim]
+        polar_coordinate - [batch, num_proposal, num_neighbor, in_feat_dim]
         """
-    
+        
+        num_neighbor = in_feat.size(1)
+
+        gk_weights = self.get_gaussian_kernel_weight(polar_coordinate) # gk_weights - [batch * num_proposal * num_neighbor, num_kernel]
+        gk_weights = gk_weights.view(-1, num_neighbor, self.num_kernel).transpose(1, 2) #gk_weights - [batch * num_proposal, num_kernel, num_neighbor]
+
+        #applying gaussian weights
+        kernel_output = torch.bmm(gk_weights, in_feat) # kernel_output - [batch * num_proposal, num_kernel, in_feat_dim]
+        kernel_output = [self.kernel_layers[i](kernel_output[:,i]) for i in range(self.num_kernel)]
+        output = torch.cat([k_output for k_output in kernel_output], dim=1)
+        output = output.view(-1, self.output_dim)  # output - [batch * num_proposal, output_dim]
+
+        return output
+
+
+
+
     def get_gaussian_kernel_weight(self, polar_coordinate):
 
         """
-        polar_coordinate - [batch, num_proposal, num_neighbor, in_feat_dim]
+        polar_coordinate - [batch, num_proposal, num_neighbor, polar_coordinate_dim]
+        weight - [batch * num_proposal * num_neighbor, num_kernel]
         """
         eps = 1e-14
         #Apply Gaussian kernel to length
@@ -80,4 +102,3 @@ class Spatial_Graph_Convolution(nn.Module):
         weight = weight / torch.sum(weight, dim=1, keepdim=True)
         
         return weight
-
